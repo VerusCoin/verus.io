d0884ed480594b91ac7e28a8c0910a113c90e9c1
Nicholas Ward <nward@foleum.io>
2020-09-12 22:09:33 +0000

    updated miningStaking links, added temp pages removed slug file

--- src/constants/miningStaking.js
+++ src/constants/miningStaking.js
@@ -1,19 +1,19 @@
 export const miningStakingLinks = [
   {
     id: 1,
-    slug: 'staking',
+    path: 'staking',
     name: 'Staking',
     description: 'Learn about staking and how to get started',
   },
   {
     id: 2,
-    slug: 'mining_software',
+    path: 'mining_software',
     name: 'Mining Software',
     description: 'Get miners and dockers for various operating systems.',
   },
   {
     id: 3,
-    slug: 'pools',
+    path: 'pools',
     name: 'Pools',
     description: 'Join mining or staking pools for consistent rewards',
   },
---
+++ src/pages/mining-and-staking/pools.js
@@ -0,0 +1,26 @@
+import Menu from '../../components/Menu';
+import { technologyLinks } from '../../constants/technology';
+import FooterMenu from '../../components/FooterMenu';
+
+const Pools = () => {
+  return (
+    <>
+      <div className="container grid max-w-5xl grid-cols-1 gap-6 md:grid-cols-4">
+        <div className="">
+          <Menu pathList={technologyLinks} href="/technology" />
+        </div>
+        <div className="max-w-5xl p-6 md:col-span-3">
+          <h1 className="p-0 m-0 text-4xl font-normal text-bluebutton">
+            Pools
+          </h1>
+          <p className="text-xl font-light leading-normal font-p">Content</p>
+          <p className="text-xl font-light leading-normal font-p">Content</p>
+          <h2 className="mt-16 text-2xl font-normal">H2</h2>
+        </div>
+      </div>
+      <FooterMenu hrefLocation="/technology" pathList={technologyLinks} />
+    </>
+  );
+};
+
+export default Pools;
