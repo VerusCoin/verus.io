d2df406086c0cbe8ba6914c49fe267a194f00818
nwardFoleum <68799661+nwardFoleum@users.noreply.github.com>
2020-11-12 10:14:55 -0600

    Update verusArticles.js
    Fix syntaxes

--- src/pages/api/verusArticles.js
+++ src/pages/api/verusArticles.js
@@ -2,8 +2,8 @@ export default async (req, res) => {
   let result = await fetch(
     'https://medium-f.herokuapp.com/api/v1/articles?orgid=veruscoin'
   );
-  let article = null
-  let data = null
+  let article = null;
+  let data = null;
   try {
       articles = await result.json();
       data = articles.articles.splice(0, 3);
