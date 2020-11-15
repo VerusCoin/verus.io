221edceb70fb48782a3d63fcf6da9dca96eec265
Nicholas Ward <nward@foleum.io>
2020-11-15 20:05:54 +0000

    issue #52

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -21,23 +21,11 @@ class MyDocument extends Document {
             sizes="180x180"
             href="/favicons/apple-touch-icon.png"
           />
+          <link rel="icon" href="/favicons/favicon.svg" type="image/svg+xml" />
           <link
-            rel="icon"
+            rel="alternate icon"
             href="/favicons/favicon.ico"
-            sizes="96x96"
-            type="image/ico"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="32x32"
-            href="/favicons/favicon-32x32.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="16x16"
-            href="/favicons/favicon-16x16.png"
+            type="image/x-icon"
           />
           <link rel="manifest" href="/favicons/site.webmanifest" />
           <meta
