d1f01855249746e37c34f0962c667cad2d4245f2
Nicholas Ward <nward@foleum.io>
2020-11-25 02:19:18 +0000

    Issue #45

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,7 @@
 export default function Custom404() {
   return (
     <div
-      className="flex items-center justify-center max-w-5xl mx-auto"
+      className="flex flex-col items-center justify-center max-w-5xl mx-auto text-left"
       style={{ height: 'calc(100vh - 350px' }}
     >
       <img
@@ -9,6 +9,7 @@ export default function Custom404() {
         alt="404"
         className="h-64"
       />
+      <p className="pr-12 text-2xl md:pr-16">404 - Page Not Found</p>
     </div>
   );
 }
