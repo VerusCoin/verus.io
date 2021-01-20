944ef55ec3b1d681611f73187a77e2d038748302
Nicholas Ward <nward@foleum.io>
2021-01-20 23:51:21 +0000

    added SubjectPage to templating

--- src/pages/technology/verus_proof_of_power.js
+++ src/pages/technology/verus_proof_of_power.js
@@ -1,5 +1,5 @@
 import { NextSeo } from 'next-seo'
-import { Menu, FooterMenu } from '@src/components'
+import { SubjectPage } from '@src/components'
 import { technologyLinks } from '@src/constants/technology'
 
 const ProofOfPower = () => {
@@ -19,90 +19,83 @@ const ProofOfPower = () => {
           },
         ]}
       />
-      <div className="container grid max-w-5xl grid-cols-1 gap-6 mt-8 mb-12 md:grid-cols-4 ">
-        <div className="">
-          <Menu pathList={technologyLinks} href="/technology" />
-        </div>
-        <div className="max-w-5xl p-6 md:col-span-3">
-          <h1 className="p-0 m-0 text-2xl font-normal md:leading-tight md:text-4xl">
-            Verus Proof of Power: Consensus Mechanism for Security
-          </h1>
-          <p className="my-8 leading-normal">
-            VerusCoin has a unique consensus mechanism: it is 50% Proof of Work
-            and 50% Proof of Stake. This means that half of newly generated
-            blocks come from mining (PoW), and the other half comes from staking
-            (PoS).
-          </p>
-          <div className="grid grid-cols-1 p-8 my-8 bg-center bg-cover rounded gap-y-12 gap-x-12 md:grid-cols-2 bg-features-bg md:gap-12">
-            <div className="flex flex-row items-center flex-shrink-0 space-x-3">
-              <img
-                src="/images/icons/50-staking.svg"
-                alt="staking image"
-                width="100"
-              />
-              <div>
-                <h4 className="text-xl text-white">Proof of Stake</h4>
-                <p className="mt-2 text-sm leading-relaxed text-theme-feature font-p">
-                  50% of all blocks are generated through staking
-                </p>
-              </div>
-            </div>
-            <div className="flex flex-row items-center flex-shrink-0 space-x-3">
-              <img
-                src="/images/icons/50-mining.svg"
-                alt="mining image"
-                width="100"
-              />
-              <div>
-                <h4 className="text-xl text-white">Proof of Work</h4>
-                <p className="mt-2 text-sm leading-relaxed text-theme-feature font-p">
-                  50% of all blocks are generated through mining
-                </p>
-              </div>
+      <SubjectPage menuList={technologyLinks} menuRef="/technology">
+        <h1 className="p-0 m-0 text-2xl font-normal md:leading-tight md:text-4xl">
+          Verus Proof of Power: Consensus Mechanism for Security
+        </h1>
+        <p className="my-8 leading-normal">
+          VerusCoin has a unique consensus mechanism: it is 50% Proof of Work
+          and 50% Proof of Stake. This means that half of newly generated blocks
+          come from mining (PoW), and the other half comes from staking (PoS).
+        </p>
+        <div className="grid grid-cols-1 p-8 my-8 bg-center bg-cover rounded gap-y-12 gap-x-12 md:grid-cols-2 bg-features-bg md:gap-12">
+          <div className="flex flex-row items-center flex-shrink-0 space-x-3">
+            <img
+              src="/images/icons/50-staking.svg"
+              alt="staking image"
+              width="100"
+            />
+            <div>
+              <h4 className="text-xl text-white">Proof of Stake</h4>
+              <p className="mt-2 text-sm leading-relaxed text-theme-feature font-p">
+                50% of all blocks are generated through staking
+              </p>
             </div>
           </div>
-          <p className="my-8 leading-normal ">
-            Verus’ staking algorithm{' '}
-            <a
-              className="underline external text-bluebutton-default"
-              href="https://medium.com/veruscoin/how-verus-solved-nothing-at-stake-and-weak-subjectivity-proof-of-stake-problems-b4dd6a85086e"
-            >
-              solves the two major theoretical issues undermining other PoS
-              systems
-            </a>
-            , “nothing at stake” and “weak subjectivity” by leveraging its smart
-            transaction capabilities to remove any incentive to attempt
-            cheating, making it a losing proposition.
-          </p>
-          <p className="my-8 leading-normal">
-            This mechanism is called Proof of Power or VerusPoP and provides a
-            further decentralizing effect on the network, incentivizing holders
-            to keep nodes online to support the network. Even if a change in
-            network hashrate happens, the PoW/PoS ratio stays the same: 50/50%.
-          </p>
-          <p className="my-8 leading-normal">
-            To successfully attack the VerusCoin blockchain, more than 50% of
-            the validation power is needed, called Chain Power. On top of that,
-            VerusCoin utilizes Komodo’s delayed Proof of Work. This means the
-            chain is constantly being notarized into the Bitcoin blockchain on
-            every bitcoin block, leveraging Bitcoin’s hashrate to help defend
-            the Verus network.
-          </p>
-          <p className="my-8 leading-normal">
-            In over two years of mainnet, VerusCoin has never experienced a
-            successful 51% hash attack.{' '}
-            <a
-              className="underline text-bluebutton-default"
-              href="/docs/VerusPoP.pdf"
-              target="_blank"
-            >
-              Learn more in the technical whitepaper of VerusPoP
-            </a>
-            .
-          </p>
+          <div className="flex flex-row items-center flex-shrink-0 space-x-3">
+            <img
+              src="/images/icons/50-mining.svg"
+              alt="mining image"
+              width="100"
+            />
+            <div>
+              <h4 className="text-xl text-white">Proof of Work</h4>
+              <p className="mt-2 text-sm leading-relaxed text-theme-feature font-p">
+                50% of all blocks are generated through mining
+              </p>
+            </div>
+          </div>
         </div>
-      </div>
-      <FooterMenu hrefLocation="/technology" pathList={technologyLinks} />
+        <p className="my-8 leading-normal ">
+          Verus’ staking algorithm{' '}
+          <a
+            className="underline external text-bluebutton-default"
+            href="https://medium.com/veruscoin/how-verus-solved-nothing-at-stake-and-weak-subjectivity-proof-of-stake-problems-b4dd6a85086e"
+          >
+            solves the two major theoretical issues undermining other PoS
+            systems
+          </a>
+          , “nothing at stake” and “weak subjectivity” by leveraging its smart
+          transaction capabilities to remove any incentive to attempt cheating,
+          making it a losing proposition.
+        </p>
+        <p className="my-8 leading-normal">
+          This mechanism is called Proof of Power or VerusPoP and provides a
+          further decentralizing effect on the network, incentivizing holders to
+          keep nodes online to support the network. Even if a change in network
+          hashrate happens, the PoW/PoS ratio stays the same: 50/50%.
+        </p>
+        <p className="my-8 leading-normal">
+          To successfully attack the VerusCoin blockchain, more than 50% of the
+          validation power is needed, called Chain Power. On top of that,
+          VerusCoin utilizes Komodo’s delayed Proof of Work. This means the
+          chain is constantly being notarized into the Bitcoin blockchain on
+          every bitcoin block, leveraging Bitcoin’s hashrate to help defend the
+          Verus network.
+        </p>
+        <p className="my-8 leading-normal">
+          In over two years of mainnet, VerusCoin has never experienced a
+          successful 51% hash attack.{' '}
+          <a
+            className="underline text-bluebutton-default"
+            href="/docs/VerusPoP.pdf"
+            target="_blank"
+          >
+            Learn more in the technical whitepaper of VerusPoP
+          </a>
+          .
+        </p>
+      </SubjectPage>
     </>
   )
 }
--- src/pages/technology/verushash.js
+++ src/pages/technology/verushash.js
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 import { NextSeo } from 'next-seo'
-import { Menu, FooterMenu } from '@src/components'
+import { SubjectPage } from '@src/components'
 import { technologyLinks } from '@src/constants/technology'
 
 const VerusHash = () => {
@@ -19,72 +19,65 @@ const VerusHash = () => {
           },
         ]}
       />
-      <div className="container grid max-w-5xl grid-cols-1 gap-6 mt-8 md:grid-cols-4">
-        <div className="">
-          <Menu pathList={technologyLinks} href="/technology" />
-        </div>
-        <div className="justify-center max-w-5xl p-6 md:col-span-3">
-          <h1 className="p-0 m-0 text-2xl font-normal md:leading-tight md:text-4xl ">
-            VerusHash: Algorithm for a Naturally Decentralized Miner Ecosystem
-          </h1>
-          <p className="my-8 leading-relaxed ">
-            VerusHash 2.2 is the mining algorithm for Verus. The algorithm was
-            developed to equalize mining equitability across hardware classes,
-            and to create a naturally decentralized miner ecosystem.
-          </p>
-          <p className="my-8 leading-relaxed">
-            No specialized mining equipment is necessary to mine Verus, and it
-            can even be solo-mined directly from the Verus Desktop wallet with
-            just a few clicks, without any additional software requirements.
-            Beginners can find an even level playing field to start earning
-            Verus.
-          </p>
-          <div className="grid grid-cols-1 p-8 mt-10 mb-2 bg-center bg-cover rounded bg-features-bg md:grid-cols-2 md:gap-6">
-            <div className="flex flex-row items-center flex-shrink-0">
-              <img src="/images/icons/CPU.png" alt="CPU image" width="100" />
-              <p className="pl-3 text-lg font-semibold text-white font-p">
-                Mining with a CPU is most profitable
-              </p>
-            </div>
-            <div className="flex flex-row items-center flex-shrink-0">
-              <img src="/images/icons/GPU.png" alt="GPU image" width="100" />
-              <p className="pl-3 text-lg font-semibold text-white font-p">
-                Mining with a GPU can be profitable
-              </p>
-            </div>
-            <div className="flex flex-row items-center flex-shrink-0">
-              <img src="/images/icons/FPGA.png" alt="FPGA image" width="100" />
-              <p className="pl-3 text-lg font-semibold text-white font-p">
-                There are no Bitstreams available to mine on FPGA's
-              </p>
-            </div>
-            <div className="flex flex-row items-center">
-              <img src="/images/icons/ASIC.png" alt="ASIC image" width="100" />
-              <p className="pl-3 text-lg font-semibold text-white font-p">
-                No ASICs are made (yet) to mine Verus
-              </p>
-            </div>
+      <SubjectPage menuList={technologyLinks} menuRef="/technology">
+        <h1 className="p-0 m-0 text-2xl font-normal md:leading-tight md:text-4xl ">
+          VerusHash: Algorithm for a Naturally Decentralized Miner Ecosystem
+        </h1>
+        <p className="my-8 leading-relaxed ">
+          VerusHash 2.2 is the mining algorithm for Verus. The algorithm was
+          developed to equalize mining equitability across hardware classes, and
+          to create a naturally decentralized miner ecosystem.
+        </p>
+        <p className="my-8 leading-relaxed">
+          No specialized mining equipment is necessary to mine Verus, and it can
+          even be solo-mined directly from the Verus Desktop wallet with just a
+          few clicks, without any additional software requirements. Beginners
+          can find an even level playing field to start earning Verus.
+        </p>
+        <div className="grid grid-cols-1 p-8 mt-10 mb-2 bg-center bg-cover rounded bg-features-bg md:grid-cols-2 md:gap-6">
+          <div className="flex flex-row items-center flex-shrink-0">
+            <img src="/images/icons/CPU.png" alt="CPU image" width="100" />
+            <p className="pl-3 text-lg font-semibold text-white font-p">
+              Mining with a CPU is most profitable
+            </p>
           </div>
-          <p className="text-sm leading-normal text-gray-600 font-p">
-            For actual hashrate statistics reported by our community see Discord
-            channels: #cpu-hashrate, #gpu-hashrate, #fpga-hashrate.
-          </p>
-          <div className="justify-center w-full mt-16 mb-32 space-y-4">
-            <Link href="/wallet/desktop-wallet">
-              <button className="w-full px-12 py-5 text-white border-0 rounded-full text-p bg-bluebutton-default hover:bg-bluebutton-hover">
-                Start solo-mining with Verus Desktop.{' '}
-                <span className="text-span-4">Easy, in just a few clicks</span>
-              </button>
-            </Link>
-            <Link href="/mining-and-staking/mining_software">
-              <button className="w-full px-12 py-5 bg-transparent border border-solid rounded-full text-p border-bluetrans-default hover:border-bluebutton-default text-bluebutton-default">
-                Get mining software
-              </button>
-            </Link>
+          <div className="flex flex-row items-center flex-shrink-0">
+            <img src="/images/icons/GPU.png" alt="GPU image" width="100" />
+            <p className="pl-3 text-lg font-semibold text-white font-p">
+              Mining with a GPU can be profitable
+            </p>
+          </div>
+          <div className="flex flex-row items-center flex-shrink-0">
+            <img src="/images/icons/FPGA.png" alt="FPGA image" width="100" />
+            <p className="pl-3 text-lg font-semibold text-white font-p">
+              There are no Bitstreams available to mine on FPGA's
+            </p>
+          </div>
+          <div className="flex flex-row items-center">
+            <img src="/images/icons/ASIC.png" alt="ASIC image" width="100" />
+            <p className="pl-3 text-lg font-semibold text-white font-p">
+              No ASICs are made (yet) to mine Verus
+            </p>
           </div>
         </div>
-      </div>
-      <FooterMenu hrefLocation="/technology" pathList={technologyLinks} />
+        <p className="text-sm leading-normal text-gray-600 font-p">
+          For actual hashrate statistics reported by our community see Discord
+          channels: #cpu-hashrate, #gpu-hashrate, #fpga-hashrate.
+        </p>
+        <div className="justify-center w-full mt-16 mb-32 space-y-4">
+          <Link href="/wallet/desktop-wallet">
+            <button className="w-full px-12 py-5 text-white border-0 rounded-full text-p bg-bluebutton-default hover:bg-bluebutton-hover">
+              Start solo-mining with Verus Desktop.{' '}
+              <span className="text-span-4">Easy, in just a few clicks</span>
+            </button>
+          </Link>
+          <Link href="/mining-and-staking/mining_software">
+            <button className="w-full px-12 py-5 bg-transparent border border-solid rounded-full text-p border-bluetrans-default hover:border-bluebutton-default text-bluebutton-default">
+              Get mining software
+            </button>
+          </Link>
+        </div>
+      </SubjectPage>
     </>
   )
 }
