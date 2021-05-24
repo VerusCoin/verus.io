e5521e822a5cdadc315c04b1e330e25c62fc8c52
Nicholas Ward <nward@foleum.io>
2021-05-24 20:18:53 +0000

    added component file and updated .gitignore

--- components/sections/Create/index.ts
+++ components/sections/Create/index.ts
@@ -1,2 +1,2 @@
 export { default as StartAProject } from './StartAProject/StartAProject'
-export { default as SelfSovereign } from './Sovereign/Sovereign'
+export { default as SelfSovereign } from './SelfSovereign/Sovereign'
