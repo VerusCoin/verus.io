ff627d2398691000c996b1a019e859fa2ccbb06a
nwardFoleum <68799661+nwardFoleum@users.noreply.github.com>
2021-10-21 08:38:35 -0500

    Update PaperCards.tsx
    fix hyperlink for downloading papers

--- components/sections/Papers/PaperCards.tsx
+++ components/sections/Papers/PaperCards.tsx
@@ -79,7 +79,7 @@ const PaperCards = ({ paper }: { paper: IPaper }) => {
         </div>
       </div>
       <StyledCardFooter>
-        <StyledDownload href="{`/papers/${file}`}" download>
+        <StyledDownload href={`/papers/${file}`} download>
           <StyledSVG>
             <SVGs name="download" />
           </StyledSVG>
