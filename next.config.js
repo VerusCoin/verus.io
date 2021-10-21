a2e1b46d17b44a537b618ae50579b84468d84de6
Nicholas Ward <nward@foleum.io>
2021-10-21 01:53:32 +0000

    fixed redirect bug 	modified:   next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 const nextTranslate = require('next-translate')
 
 module.exports = nextTranslate({
-  images: {
-    domains: ['cdn-images-1.medium.com'],
-  },
-  async redirect() {
+  async redirects() {
     return [
       {
         source: '/techonolgy',
@@ -78,5 +75,9 @@ module.exports = nextTranslate({
         permanent: true,
       },
     ]
+  },
+
+  images: {
+    domains: ['cdn-images-1.medium.com'],
   },
 })
