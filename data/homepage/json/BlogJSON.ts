862147cdab7d14114d7339d9d5a07cd2c3561042
Nicholas Ward <nward@foleum.io>
2021-11-20 00:33:16 +0000

    updated highlighted articles

--- data/homepage/json/BlogJSON.ts
+++ data/homepage/json/BlogJSON.ts
@@ -5,17 +5,18 @@ export const BlogJSON: IBlog = {
   data: [
     {
       thumbnail:
-        'https://cdn-images-1.medium.com/max/1024/1*OGyWPTg42puvdReaOmY7sQ.png',
-      title: 'Verus — Superior DeFi for Community Driven Liquidity',
-      pubDate: '2020-10-03T21:45:48Z',
-      link: 'https://medium.com/veruscoin/verus-superior-defi-for-community-driven-liquidity-e2592b251146',
-    },
-    {
-      thumbnail:
         'https://cdn-images-1.medium.com/max/1024/1*mM9OwDNz-t2F5ZA2N1o1Jw.png',
       title: 'Verus: Profit Generating Protocol for Miners and Stakers',
       pubDate: '2021-05-11T19:50:08Z',
       link: 'https://medium.com/veruscoin/verus-profit-generating-protocol-for-miners-and-stakers-2d2b454aa330',
+    },
+    {
+      thumbnail:
+        'https://cdn-images-1.medium.com/max/1024/1*5N-bBDKLJZr5nBAYzNvGKA.png',
+      title:
+        'Verus Announces New Multicurrency, Multichain DeFi Protocols and Immediate Public Availability of Fully Functional, Multichain Testnet',
+      pubDate: '2021-05-18T17:29:24Z',
+      link: 'https://medium.com/veruscoin/verus-announces-new-multicurrency-multichain-defi-protocols-and-immediate-public-availability-of-f1868f259ce5',
     },
     {
       thumbnail:
