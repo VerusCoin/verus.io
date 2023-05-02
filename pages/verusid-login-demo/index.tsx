a0326776edb0f3e9378b91930b1cac0288961ba3
naward-cs <nicholas.ward.cyber@gmail.com>
2023-05-01 18:04:53 -0700

    added mobile device detect

--- pages/verusid-login-demo/index.tsx
+++ pages/verusid-login-demo/index.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-
+import { isMobile } from 'react-device-detect'
 import io from 'socket.io-client'
 import styled from 'styled-components'
 // import { QRCode as QrCode } from 'react-qrcode-logo'
@@ -9,15 +9,15 @@ import QRCode from 'react-qr-code'
 import { StyledContainer } from '@/components/sections/VerusId/Styles'
 import { spacer } from '@/styles/helpers'
 import { media } from 'styled-bootstrap-grid'
-import { DefaultText } from '@/components/elements'
+import { Button, DefaultText } from '@/components/elements'
 import { FcApproval } from 'react-icons/fc'
 let socket
-// const StyledQRCode = styled.div`
-//   margin: auto;
-//   padding: 20px;
-//   border: solid 1px;
-//   border-radius: 5px;
-// `
+const StyledQRCode = styled.div`
+  margin: auto;
+  padding: 20px;
+  border: solid 1px;
+  border-radius: 5px;
+`
 const StyledDiv = styled.div<any>`
   ${(props) => (props.bottom ? spacer('xl') : spacer('xxl'))}
   justify-content: center;
@@ -39,13 +39,13 @@ const StyledDiv = styled.div<any>`
   `}
 `
 
-const StyledQRLink = styled.a<any>`
-  margin: auto;
-  padding: 20px;
-  border: solid 1px black;
-  border-radius: 5px;
-  text-decoration: none;
-`
+// const StyledQRLink = styled.a<any>`
+//   margin: auto;
+//   padding: 20px;
+//   border: solid 1px black;
+//   border-radius: 5px;
+//   text-decoration: none;
+// `
 
 const StyledNote = styled.div`
   grid-column: span 2;
@@ -59,7 +59,7 @@ const VerusIdLoginExample = () => {
   const [user, setUser] = useState<string>()
   const [success, setSuccess] = useState(false)
   const [session_key, setSession_key] = useState<string>()
-  const title = 'VerusId Login Example'
+  const title = 'VerusID Login Example'
   const description = 'Test using the VerusID as Login Credentials.'
 
   useEffect(() => {
@@ -135,51 +135,42 @@ const VerusIdLoginExample = () => {
         ) : (
           <StyledContainer>
             <StyledDiv>
-              <StyledQRLink href={qrRedirURL}>
-                {qrHookURL ? (
-                  <>
-                    {/* <QrCode
-                      value={qrURL}
-                      size={300}
-                      logoImage={`./svg/verus-logo.svg`}
-                      logoOpacity={0.25}
-                      logoWidth={150}
-                      qrStyle="dots"
-                      ecLevel="Q"
-                    /> */}
-                    <QRCode value={qrHookURL} level="Q" size={300} />
-                    <DefaultText align="center" fontSz="xs" customColor="black">
-                      Click or scan QR-Code
-                    </DefaultText>
-                  </>
-                ) : (
-                  <DefaultText align="center" fontSz="md">
-                    ...Getting Login Request
+              {isMobile ? (
+                <>
+                  <DefaultText align="center">
+                    <Button href={qrRedirURL}>Login with VerusID</Button>
                   </DefaultText>
-                )}
-              </StyledQRLink>
-              {/* <StyledQRCode>
-                {qrURL ? (
-                  <QRCode value={qrURL} level="Q" size={300} />
-                ) : (
                   <DefaultText align="center" fontSz="md">
-                    ...Getting Login Request
+                    VerusID Login Example
                   </DefaultText>
-                )}
-              </StyledQRCode> */}
+                  <DefaultText align="center" fontSz="sm">
+                    Click to login in using your VerusID on your Mobile App.
+                  </DefaultText>
+                </>
+              ) : (
+                <>
+                  <StyledQRCode>
+                    {qrHookURL ? (
+                      <QRCode value={qrHookURL} level="Q" size={300} />
+                    ) : (
+                      <DefaultText align="center" fontSz="md">
+                        ...Getting Login Request
+                      </DefaultText>
+                    )}
+                  </StyledQRCode>
+                  <DefaultText align="center" fontSz="md">
+                    VerusID Login Example
+                  </DefaultText>
+                  <DefaultText align="center" fontSz="sm">
+                    Scan the QR Code to login in using your VerusID on your
+                    Mobile App.
+                  </DefaultText>
+                </>
+              )}
 
-              <DefaultText align="center" fontSz="md">
-                VerusId Login Example
-              </DefaultText>
-              <DefaultText align="center" fontSz="sm">
-                Scan the QR Code to login in, or click on QR-Code if Verus App
-                is installed with a VerusID
-              </DefaultText>
               <StyledNote>
                 <DefaultText align="center" fontSz="xs">
-                  NOTE: This is an demo and not a full authentication. Is not
-                  configured to work on Mobile Browswer at this time. Test
-                  scanning from desktop browswer only.
+                  NOTE: This is a demo and not a full authentication process.
                 </DefaultText>
               </StyledNote>
             </StyledDiv>
@@ -191,3 +182,33 @@ const VerusIdLoginExample = () => {
 }
 
 export default VerusIdLoginExample
+
+{
+  /* <StyledQRLink href={qrRedirURL}>
+                  {qrHookURL ? (
+                    <>
+                      {/* <QrCode
+                      value={qrURL}
+                      size={300}
+                      logoImage={`./svg/verus-logo.svg`}
+                      logoOpacity={0.25}
+                      logoWidth={150}
+                      qrStyle="dots"
+                      ecLevel="Q"
+                    /> */
+}
+//       <QRCode value={qrHookURL} level="Q" size={300} />
+//       <DefaultText
+//         align="center"
+//         fontSz="xs"
+//         customColor="black"
+//       >
+//         Click or scan QR-Code
+//       </DefaultText>
+//     </>
+//   ) : (
+//     <DefaultText align="center" fontSz="md">
+//       ...Getting Login Request
+//     </DefaultText>
+//   )}
+// </StyledQRLink> */}
