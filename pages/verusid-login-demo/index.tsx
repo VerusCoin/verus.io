917c04fb0b66204b37a144f157d373d354c64547
naward-cs <nicholas.ward.cyber@gmail.com>
2023-05-04 15:35:13 -0700

    finish testing

--- pages/verusid-login-demo/index.tsx
+++ pages/verusid-login-demo/index.tsx
@@ -1,9 +1,7 @@
 import { useEffect, useState } from 'react'
-// import { GetServerSideProps } from 'next'
 import { isMobile } from 'react-device-detect'
-// import io from 'socket.io-client'
 import styled from 'styled-components'
-// import { QRCode as QrCode } from 'react-qrcode-logo'
+
 import { MainLayout } from '@/components/layouts'
 import { NextSeo } from 'next-seo'
 import QRCode from 'react-qr-code'
@@ -42,14 +40,6 @@ const StyledDiv = styled.div<any>`
   `}
 `
 
-// const StyledQRLink = styled.a<any>`
-//   margin: auto;
-//   padding: 20px;
-//   border: solid 1px black;
-//   border-radius: 5px;
-//   text-decoration: none;
-// `
-
 const StyledNote = styled.div`
   grid-column: span 2;
   margin: auto;
@@ -81,13 +71,11 @@ const StyledButton = styled.a<any>`
 const fetcher = (url: string) => fetch(url).then((res) => res.json())
 
 const VerusIdLoginExample = () => {
-  // const [qrHookURL, setQrHookURL] = useState()
-  // const [qrRedirURL, setQrRedirURL] = useState()
   const [user, setUser] = useState<string>()
   const [success, setSuccess] = useState(false)
   const [getKey, setGetKey] = useState(false)
   const [start, setStart] = useState(true)
-  // const [session_key, setSession_key] = useState<string>()
+
   const title = 'VerusID Login Example'
   const description = 'Test using the VerusID as Login Credentials.'
   const { data: loginConsent } = useSWR(
@@ -96,13 +84,7 @@ const VerusIdLoginExample = () => {
       : undefined,
     fetcher
   )
-  //  const { data: verification } = useSWR(
-  //    start && session_key ? start : undefined,
-  //    fetcher,
-  //    {
-  //      refreshInterval: 6000,
-  //    }
-  //  )
+
   const { data: verification } = useSWR(
     () =>
       start
@@ -118,40 +100,8 @@ const VerusIdLoginExample = () => {
     setGetKey(true)
   }, [])
 
-  // useEffect(() => {
-  //   if (loginConsent) {
-  //     setSession_key(loginConsent.hook.session_key)
-  //     setStart('/api/auth/verusIdLoginStatus?session=' + session_key)
-  //     console.warn('session', session_key)
-  //     console.warn('url', start)
-  //     setQrHookURL(loginConsent.hook.uri)
-  //     setQrRedirURL(loginConsent.redir.uri)
-  //   }
-  // }, [loginConsent])
-
-  // const getLoginConsentRequest = useCallback(async () => {
-  //   const data = await fetch(
-  //     `/api/auth/verusIdLogin?redir=${window.location.href}&hook=${window.origin}`
-  //   ).then((res) => res.json())
-
-  //   setSession_key(data.hook.session_key)
-  //   setStart('/api/auth/verusIdLoginStatus?session=' + session_key)
-  //   console.warn('session', session_key)
-  //   console.warn('url', start)
-  //   setQrHookURL(data.hook.uri)
-  //   setQrRedirURL(data.redir.uri)
-  // }, [])
-
   useEffect(() => {
-    console.warn('data', verification)
     if (verification) {
-      console.warn('session_key', loginConsent.hook.session_key)
-      console.warn('data.session', verification.session)
-      console.warn(
-        'session match',
-        verification.session === loginConsent.hook.session_key
-      )
-      console.warn('isValid', verification.valid)
       if (
         verification.session === loginConsent.hook.session_key &&
         verification.valid
@@ -250,54 +200,4 @@ const VerusIdLoginExample = () => {
   )
 }
 
-// export const getServerSideProps: GetServerSideProps = async (ctx: any) => {
-//   const loginResponse = ctx.query as {
-//     session: string
-//     i5fvfsaTFKtrHCPYQHTXRaXcyxHmJMxTMe: string
-//   }
-//   const params = new URLSearchParams(loginResponse)
-//   if (params) {
-//     // console.log(loginResponse.i5fvfsaTFKtrHCPYQHTXRaXcyxHmJMxTMe)
-//     console.log(params)
-//     // console.log(loginResponse.i5fvfsaTFKtrHCPYQHTXRaXcyxHmJMxTMe)
-//   }
-
-//   // const result = await fetch(
-//   //   `http://localhost:3000/api/auth/verifyLogin?${params}`
-//   // ).then((res) => res.json())
-//   return {
-//     props: {},
-//   }
-// }
-
 export default VerusIdLoginExample
-
-{
-  /* <StyledQRLink href={qrRedirURL}>
-                  {qrHookURL ? (
-                    <>
-                      {/* <QrCode
-                      value={qrURL}
-                      size={300}
-                      logoImage={`./svg/verus-logo.svg`}
-                      logoOpacity={0.25}
-                      logoWidth={150}
-                      qrStyle="dots"
-                      ecLevel="Q"
-                    /> */
-}
-//       <QRCode value={qrHookURL} level="Q" size={300} />
-//       <DefaultText
-//         align="center"
-//         fontSz="xs"
-//         customColor="black"
-//       >
-//         Click or scan QR-Code
-//       </DefaultText>
-//     </>
-//   ) : (
-//     <DefaultText align="center" fontSz="md">
-//       ...Getting Login Request
-//     </DefaultText>
-//   )}
-// </StyledQRLink> */}
